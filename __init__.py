9f9108154bb69658a77bf51be0c20b14797beff6
James Dunne <66116291+dunneja@users.noreply.github.com>
2022-10-21 23:15:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from gui import gui_prntool
-from gui import csv_viewer
+from gui import gui_csv_view
+from gui import gui_func
+from gui import gui_help
 
 from core import prn_connc
 from core import prn_datac

534b19d5a116a419eacec46c3ba01bddd961e073
James Dunne <66116291+dunneja@users.noreply.github.com>
2022-10-21 22:30:18 +0100

    Refactoring

--- functions/log_func.py
+++ functions/log_func.py
@@ -10,22 +10,33 @@
 from datetime import datetime
 
 def logw(stdout_lfn, output):
+    
     """
     function to write output to txt log file.
     """
+    
     #log file date time format
     file_dt = datetime.now().strftime("%d-%m-%Y")
+    
     #log file directory
     log_dir = 'logs'
+    
     #log file format
     log_format = '.txt'
+    
     #stdout final log file
     stdout_lf = log_dir + '/' + stdout_lfn + '_' + file_dt + log_format
+    
     #write stdout list to log file
     try:
+        
         with open(stdout_lf, 'a') as fout:
+            
             fout.write(
                 str(datetime.now().strftime("%d-%m-%Y_%I-%M-%S-%p")))
+            
             fout.write(" %s\n" % output)
+            
     except FileNotFoundError:
+        
         print(' * Error: No such file or directory!')

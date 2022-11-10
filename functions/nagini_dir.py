530b31801994f5d038d70fdf8ef07d2627176bd9
James Dunne <james.dunne1@gmail.com>
2022-11-10 20:53:57 +0000

    Update nagini_dir.py

--- functions/nagini_dir.py
+++ functions/nagini_dir.py
@@ -13,63 +13,36 @@
 import os
 from functions import nagini_log
 
-
 def chk_log_dir(log_dir):
-    
     """
     check if the log dir exists if not make.
     """
-    
-    if (os.path.isdir(log_dir)):
-        
+    if (os.path.isdir(log_dir)):    
         print(f' * {log_dir} directory exists!')
-        
     else:
-        
-        try:
-            
+        try:   
             print(f" * {log_dir} directory doesn't exist!")
-            
             os.mkdir(log_dir)
-            
             print(f' * Sucessfully created the {log_dir} directory.')
-            
             emsg = f'Sucessfully created the {log_dir} directory.'
-            
             nagini_log.logw('sys_log', emsg)
-            
         except OSError:
-            
             print(f' * Creation of the {log_dir} directory failed.')
 
 def chk_output_dir(output_dir):
-    
     """
     check if the output dir exists if not make.
     """
-    
-    if (os.path.isdir(output_dir)):
-        
+    if (os.path.isdir(output_dir)): 
         print(f' * {output_dir} directory exists!')
-        
     else:
-        
-        try:
-            
+        try:   
             print(f" * {output_dir} directory doesn't exist!")
-            
             os.mkdir(output_dir)
-            
             print(f' * Sucessfully created the {output_dir} directory.')
-            
             emsg = f'Sucessfully created the {output_dir} directory.'
-            
             nagini_log.logw('sys_log', emsg)
-            
         except OSError:
-            
             print(f' * Creation of the {output_dir} directory failed.')
-            
             emsg = f' * Creation of the {output_dir} directory failed.'
-            
             nagini_log.logw('sys_log', emsg)
